f02f37cac29c07b6a93d2ef4aa4c8d14acd21a12
Irvin Chu <irvin.chu@toplinegamelabs.com>
2014-12-11 13:36:17 -0800

    rails assets

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(:default, Rails.env)
 
 module ToglAdmin
   class Application < Rails::Application
+    # heroku - serve up assets
+    config.serve_static_assets = true
+
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
