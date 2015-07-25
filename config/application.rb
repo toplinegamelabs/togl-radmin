ab76207354b0470223afd5011df6a19c68e6c3e2
Irvin Chu <irvin.chu@toplinegamelabs.com>
2015-07-24 17:02:45 -0700

    help with assets on heroku II

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(:default, Rails.env)
 module ToglAdmin
   class Application < Rails::Application
     config.serve_static_assets = true
+    config.assets.paths << Rails.root.join("app", "assets", "javascripts", "promotions")
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
