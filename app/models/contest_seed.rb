ca0b0603101deb59982a352d294051b522d549e1
Joe Trdinich <joe.trdinich@toplinegamelabs.com>
2015-08-17 16:32:20 -0400

    Add contest_seeds table and modle

---
+++ app/models/contest_seed.rb
@@ -0,0 +1,18 @@
+class ContestSeed < ActiveRecord::Base
+  validates_presence_of :game_identifier
+  validates_presence_of :size
+  validates_presence_of :buy_in
+  validates_presence_of :minutes_before_lock_cutoff
+
+  validates_uniqueness_of :game_identifier, scope: [:size, :buy_in]
+
+  before_create :set_defaults, on: :create
+
+
+  private
+
+  def set_defaults
+    self.minutes_before_lock_cutoff = 60 if self.minutes_before_lock_cutoff.nil?
+    self.is_active = true if self.is_active.nil?
+  end
+end
