6cbe14a52adde7f7dee7fa04663972dd74fe5846
Joe Trdinich <joe.trdinich@toplinegamelabs.com>
2015-08-19 00:28:26 -0400

    Check for non-full contests

--- lib/contest_hashie.rb
+++ lib/contest_hashie.rb
@@ -1,12 +1,14 @@
 class ContestHashie < Hashie::Dash
   property :contest_template
   property :is_invite_only
+  property :user_contests_count
 
   def self.build_from_rapi_hash(hash)
     contest = self.new
 
     contest.contest_template = ContestTemplateHashie.build_from_rapi_hash(hash["contest_template"])
     contest.is_invite_only = hash["is_invite_only"]
+    contest.user_contests_count = hash["user_contests_count"]
 
     contest
   end
